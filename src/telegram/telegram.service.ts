37422dab0b6bba6ff4d427f95b1de3e0427ae80f
sthm23 <sanjikk@mail.ru>
2023-12-20 21:09:40 +0500

    fix: add token

--- src/telegram/telegram.service.ts
+++ src/telegram/telegram.service.ts
@@ -6,7 +6,7 @@ import axios from 'axios';
 import { CarsList } from "./interfaces";
 import { Cron, CronExpression } from '@nestjs/schedule';
 
-const token = '';
+const token = '6847344625:AAFNi7WErznuhli8GtQCZDN2CHaqCqwhToU';
 const BaseUrl = 'https://savdo.uzavtosanoat.uz/b/ap/stream/ph&models';
 
 @Injectable()
